4e0ccd0153379893a310f2c73c9e78c89ec2d749
lsalamone22 <salamoneloreley@hotmail.com>
2022-08-26 12:25:27 -0500

    cambios exitosos

--- Models/IGV.cs
+++ Models/IGV.cs
@@ -2,17 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace webapp.Models
 {
     public class IGV
     {
+        [Display( Name = "Nombre")]
+       
         public string Operador1 {get;set;} 
 
+        [Display( Name = "Kilogramos")]
         public int Operador2 {get;set;} 
 
+        [Display( Name = "Unidad", Prompt ="Ingrese unidad")]
          public string Operador3 {get;set;} 
 
+        [Display( Name = "Precio")]
          public int Pre {get;set;}
     }
 }
